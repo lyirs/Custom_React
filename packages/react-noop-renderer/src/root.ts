b1fccda18f86239341f436887c268169ec5e57bd
lv <992526373@qq.com>
2023-05-06 18:32:25 +0800

    feat:Noop-Renderer 以ReactElement的形式导出树状结构

--- packages/react-noop-renderer/src/root.ts
+++ packages/react-noop-renderer/src/root.ts
@@ -1,10 +1,11 @@
 // ReactDOM.createRoot(root).render(<App />);
 
-import { Instance } from 'hostConfig';
+import { Instance } from './hostConfig';
 import {
 	createContainer,
 	updateContainer
 } from 'react-reconciler/src/fiberReconciler';
+import { REACT_ELEMENT_TYPE, REACT_FRAGMENT_TYPE } from 'shared/ReactSymbols';
 import { ReactElementType } from 'shared/ReactTypes';
 import { Container } from './hostConfig';
 
@@ -25,12 +26,78 @@ export const createRoot = () => {
 		return null;
 	};
 
+	const getChildrenAsJSX = (root: Container) => {
+		const children = childToJSX(getChildren(root));
+		if (Array.isArray(children)) {
+			return {
+				$$typeof: REACT_ELEMENT_TYPE,
+				type: REACT_FRAGMENT_TYPE,
+				key: null,
+				ref: null,
+				props: { children },
+				__mark: '与真实的React区分的字段'
+			};
+		}
+		return children;
+	};
+
+	const childToJSX = (child: any): any => {
+		if (typeof child === 'string' || typeof child === 'number') {
+			return child;
+		}
+
+		if (Array.isArray(child)) {
+			if (child.length === 0) {
+				return null;
+			}
+			if (child.length === 1) {
+				return childToJSX(child[0]);
+			}
+			const children = child.map(childToJSX);
+
+			if (
+				children.every(
+					(child) => child === 'string' || typeof child === 'number'
+				)
+			) {
+				return children.join('');
+			}
+			return children;
+		}
+
+		// Instance
+		if (Array.isArray(child.children)) {
+			const instance: Instance = child;
+			const children = childToJSX(instance.children);
+			const props = instance.props;
+
+			if (children !== null) {
+				props.children = children;
+			}
+
+			return {
+				$$typeof: REACT_ELEMENT_TYPE,
+				type: instance.type,
+				key: null,
+				ref: null,
+				props,
+				__mark: '与真实的React区分的字段'
+			};
+		}
+
+		// TextInstance
+		return child.text;
+	};
+
 	return {
 		render(element: ReactElementType) {
 			return updateContainer(element, root);
 		},
 		getChildren() {
 			return getChildren(container);
+		},
+		getChildrenAsJSX() {
+			return getChildrenAsJSX(container);
 		}
 	};
 };
