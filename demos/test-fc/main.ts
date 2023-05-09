97e0df9cae63b9be4a393ff7c3f48c3ae52e80fe
lv <992526373@qq.com>
2023-05-09 15:16:38 +0800

    feat: 实现同步更新Demo

--- demos/test-fc/main.ts
+++ demos/test-fc/main.ts
@@ -1,3 +1,10 @@
+// 交互触发更新
+// 1.调度阶段微任务调度（ensureRootIsScheduled方法）
+// 2.微任务调度结束，进入render阶段
+// 3.render阶段结束，进入commit阶段
+// 4.commit阶段结束，调度阶段微任务调度（ensureRootIsScheduled方法）
+// 5.整体是个大的微任务循环，循环的驱动力是「微任务调度模块」。
+
 import './style.css';
 
 const button = document.querySelector('button');
@@ -20,7 +27,7 @@ const schedule = () => {
 const perform = (work: Work) => {
 	while (work.count) {
 		work.count--;
-		insertSpan('0 ');
+		insertSpan('0');
 	}
 	schedule();
 };
@@ -38,3 +45,8 @@ button &&
 		});
 		schedule();
 	});
+
+// 示例在两种情况下会造成阻塞：
+
+// 1.work.count数量太多
+// 2.单个work.count工作量太大
