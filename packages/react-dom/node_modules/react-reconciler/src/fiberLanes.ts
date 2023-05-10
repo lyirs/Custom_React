d23d11edfd648cd00416d3fc3da8a35607067909
lv <992526373@qq.com>
2023-05-10 14:09:08 +0800

    feat: 实现并发更新的交互部分

--- packages/react-dom/node_modules/react-reconciler/src/fiberLanes.ts
+++ packages/react-dom/node_modules/react-reconciler/src/fiberLanes.ts
@@ -1,3 +1,10 @@
+import {
+	unstable_getCurrentPriorityLevel,
+	unstable_IdlePriority,
+	unstable_ImmediatePriority,
+	unstable_NormalPriority,
+	unstable_UserBlockingPriority
+} from 'scheduler';
 import { FiberRootNode } from './fiber';
 
 export type Lane = number;
@@ -6,13 +13,19 @@ export type Lanes = number;
 export const SyncLane = 0b0001;
 export const NoLane = 0b0000;
 export const NoLanes = 0b0000;
+export const InputContinousLane = 0b0010;
+export const DefaultLane = 0b0100;
+export const IdleLane = 0b1000;
 
 export const mergeLanes = (laneA: Lane, laneB: Lane): Lanes => {
 	return laneA | laneB;
 };
 
 export const requestUpdateLanes = () => {
-	return SyncLane;
+	// 从上下文环境中获取Scheduler优先级
+	const currentSchedulerPriority = unstable_getCurrentPriorityLevel();
+	const lane = schedulerPriorityToLane(currentSchedulerPriority);
+	return lane;
 };
 
 export const getHighestPriorityLane = (lanes: Lanes): Lane => {
@@ -21,4 +34,31 @@ export const getHighestPriorityLane = (lanes: Lanes): Lane => {
 
 export const markRootFinished = (root: FiberRootNode, lane: Lane) => {
 	root.pendingLanes &= ~lane;
+};
+
+export const lanesToSchedulerPriority = (lanes: Lanes) => {
+	const lane = getHighestPriorityLane(lanes);
+	if (lane === SyncLane) {
+		return unstable_ImmediatePriority;
+	}
+	if (lane === InputContinousLane) {
+		return unstable_UserBlockingPriority;
+	}
+	if (lane === DefaultLane) {
+		return unstable_NormalPriority;
+	}
+	return unstable_IdlePriority;
+};
+
+export const schedulerPriorityToLane = (schedulerPriority: number): Lane => {
+	if (schedulerPriority === unstable_ImmediatePriority) {
+		return SyncLane;
+	}
+	if (schedulerPriority === unstable_UserBlockingPriority) {
+		return InputContinousLane;
+	}
+	if (schedulerPriority === unstable_NormalPriority) {
+		return DefaultLane;
+	}
+	return NoLane;
 };
